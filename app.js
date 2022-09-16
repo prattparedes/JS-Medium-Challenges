cd5d59a92863d75120d8576803cd5f0e8fc0de3c
Pratt <pratt.paredes@gmail.com>
2022-09-15 20:02:19 -0500

    convert to boolean, filter falsys and truthys, and more

--- app.js
+++ app.js
@@ -108,10 +108,107 @@ function reverseString2(str) {
 
 console.log(reverseString2('Thomas'))
 
-//Método array reverse .spli(''), reverse(), join('')
+//Método array reverse .split(''), reverse(), join('')
 function reverseString3 (str) {
     return newstr = str.split('').reverse().join('')
 }
 
 console.log(reverseString3('Frontend is the Best'))
+
+// Transforma cada elemento de un array en 0
+//1er método con FOR LOOP
+
+function converttoZeros1(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = 0
+    }
+    return arr
+}
+
+console.log(converttoZeros1([2, 5, 12]))
+
+//2do metodo array "fill"
+function converttoZeros2(arr) {
+    return new Array(arr.length).fill(0)
+}
+
+console.log(converttoZeros2([5, 10, 15, 12, 1]))
+
+//3er método "map"
+
+function converttoZeros3(arr) {
+    return arr.map(() => {                  // Cuando usas el {} dentro de un map si o si debes poner "return" porque si no todo quedará
+        return 0                            // como undefined
+    });
+}
+
+console.log(converttoZeros3([100, 100, 200, 300, 200, 100, 200]))
+
+//Si es un "apple" lo removemos del array
+//1er método con loop for
+function removeApples(arr) {
+    newArr = []
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== 'apple') {
+            newArr.push(arr[i])
+        }
+    }
+    return newArr
+}
+
+console.log(removeApples(['apple', 'dog', 'banana', 'apple']))
+console.log(removeApples(['apple', 'Owo', 'apple', 'orange', 5]))
+
+//2do método con filter() 
+
+function removeApples2(arr) {
+    return arr.filter(response => {
+        if (response !== 'apple') {
+            return true
+        }
+    })
+}
+
+console.log(removeApples2(['hola', 'apple', 'banana', 'apple', 'pineaple']))
+
+//2do método mas rapido
+
+function removeApples3(arr) {
+    return arr.filter(response => response !== 'apple')
+}
+
+console.log(removeApples3(['apple', 'banana', 'apple', 'pineaple', 'fruit', 'apple']))
+
+
+// Filtrar todos los falsy values
+//1er metodo loop
+function filtrarFalsy(arr) {
+    let newArr = []
+    for (let i = 0; i < arr.length; i++) {
+        if (!!arr[i]) {
+            newArr.push(arr[i])
+        }
+    }
+    return newArr
+}
+
+console.log(filtrarFalsy(['', [], 0, null, undefined, '0']))
+console.log(filtrarFalsy(['Tomato', 'Banana', false, 'orange']))
+console.log(filtrarFalsy(['', [], 0, null, undefined, '0']))
+
+//2do metodo filter
+
+function filtrarFalsy2(arr) {
+    return arr.filter(response => !!response === true)
+}
+
+console.log(filtrarFalsy2([[], '', NaN, 'orange', 5, false]))
+
+// Pasar todos los elementos de un array a su booleano
+
+function converToBoolean(arr) {
+    return arr.map(response => !!response)
+}
+
+console.log(converToBoolean([[], '', NaN, 'orange', 5, false]))
 
